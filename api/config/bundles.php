e27048276d7f103052dd343439bb924195749cf7
liszkapawel <pawel.liszka00@gmail.com>
2026-02-15 11:14:20 +0100

    Fix TwigBundle for prod

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -11,5 +11,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true],
 ];
