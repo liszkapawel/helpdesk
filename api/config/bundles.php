c6c5c5efe755b0d17f9d2dc43045ab8af1eaeb3c
liszkapawel <pawel.liszka00@gmail.com>
2026-02-16 07:19:04 +0100

    Enable fixtures bundle in all environments

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -13,5 +13,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
 ];
